3d7b23a7ab3a253026700317feaf1c0b30b9cc69
Nicholas Van Exan <nick.vanexan@gmail.com>
2017-04-01 17:51:41 -0400

    Adding css for blog and build tasks

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp          = require('gulp');
+var sass          = require('gulp-sass');
+var rename        = require('gulp-rename');
+var concat        = require('gulp-concat');
+var minify        = require('gulp-minify-css');
+var merge         = require('merge-stream');
+
+var input = "./scss/**/*.scss";
+var output = "./css";
+
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
+gulp.task('sass', function () {
+  return gulp
+  .src(input)
+  .pipe(sass(sassOptions).on('error', sass.logError))
+  .pipe(gulp.dest(output));
+});
+
+gulp.task('watch', function () {
+  return gulp
+  .watch(input, ['sass'])
+  .on('change', function (event) {
+    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+  });
+});
+
+gulp.task('css', function () {
+  var scssStream = gulp.src(input)
+    .pipe(sass())
+    .pipe(concat('scss-files.scss'));
+
+  var mergedStream = merge(scssStream)
+    .pipe(concat('blog.css'))
+    .pipe(minify())
+    .pipe(gulp.dest('css'));
+
+  return mergedStream;
+});
+
+gulp.task('default', ['sass', 'watch']);
